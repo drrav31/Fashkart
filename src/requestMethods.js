53539da5b2dbbeb53ce4e7f6a8304a25c7d8a0cc
unknown <ravitej630@gmail.com>
2022-03-28 23:28:25 +0530

    update user session

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://fashkart.herokuapp.com/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken;
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)?.currentUser?.accessToken;
+console.log(TOKEN);
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
